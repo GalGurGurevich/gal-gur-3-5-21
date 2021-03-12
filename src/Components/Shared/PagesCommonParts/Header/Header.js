b6250448e4b6fef012e92ad87c2b1b5ce5924c17
Gal Gur <gal.gurevich@gmail.com>
2021-03-12 19:41:02 +0200

    del redudndent css rules

--- src/Components/Shared/PagesCommonParts/Header/Header.js
+++ src/Components/Shared/PagesCommonParts/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Header.css'
 
-export default function Header({ txt }) {
+export default function Header({txt}) {
     return (
         <h3 className="header-root">{txt}</h3>
     )
