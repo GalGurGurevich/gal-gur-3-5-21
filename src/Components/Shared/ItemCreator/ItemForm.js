a53f1d75444ff1c69a7dc822317cc87d13ba9296
Gal Gur <gal.gurevich@gmail.com>
2021-03-20 09:25:14 +0200

    added number extras to text feild number

--- src/Components/Shared/ItemCreator/ItemForm.js
+++ src/Components/Shared/ItemCreator/ItemForm.js
@@ -15,11 +15,15 @@ const useStyles = makeStyles((theme) => ({
 export default function InputFields({setName, setStore, setPrice, setDate, name, store, price, date, nameLbl, storeLbl, priceLbl, dateLbl}) {
   const classes = useStyles();
 
+  const formatText = (text) => {
+    return text.replace(/[^+\d]/g, '')
+  }
+
   return (
     <form className={classes.root} noValidate autoComplete="off">
       <TextField onChange={e => setName(e.target.value)} type="text" id="outlined-basic" value={name} label={nameLbl} variant="outlined" />
       <TextField onChange={e => setStore(e.target.value)} type="text" id="outlined-basic" value={store} label={storeLbl} variant="outlined" />
-      <TextField onChange={e => setPrice(e.target.value)} type="number" id="outlined-basic" value={price} label={priceLbl} variant="outlined" />
+      <TextField onChange={e => setPrice(e.target.value)} type="number" formatText={formatText} keyboardType="phone-pad" id="outlined-basic" value={price} label={priceLbl} variant="outlined" />
       <TextField onChange={e => setDate(e.target.value)} type="date" id="outlined-basic" value={date} InputLabelProps={{shrink: true}} label={dateLbl} variant="outlined" />
     </form>
   );
