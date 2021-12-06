8b951187c5bdd0dd8092df9035cbbec0ac5929d6
Gal Gur <gal.gurevich@gmail.com>
2021-12-06 20:14:19 +0200

    fix api key

--- src/API/exchangeRates.js
+++ src/API/exchangeRates.js
@@ -1,6 +1,6 @@
 export default function getRates() {
     console.log("in get rates")
-    const API_KEY = 'access_key=431c5799b6e481816f75414ba2556aa1'
+    const API_KEY = 'access_key=5a1c2c9921bd564df0ec4470a9283691'
     return fetch('http://api.exchangeratesapi.io/v1/latest?base=EUR&symbols=ILS&' + API_KEY).then(response => response.json());
 }
 // https://api.exchangeratesapi.io/v1/latest
